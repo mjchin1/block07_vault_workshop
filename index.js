4b78af8607abdfe73d0d679c05766f76d334d450
Melissa J. Chin <Melissa@Melissas-MacBook-Air.local>
2023-11-06 12:58:19 -0600

    Cleaned up pseudocode and comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-//1. name three variables. 
-//2. for each variable, create an equation returns a value equal to one of the combination numbers (10, 40 and 39). Use different operators for each equation. 
-//3. Display the results of each equation in a pop-up. 
+//Pseudocode:
+//1. Create a string containing the pop-up message. 
+//2. Declare three variables. 
+//3. For each variable, create an equation that returns a value equal to one of the combination numbers (10, 40 and 39). Use different operators for each equation. 
+//4. Display a pop-up message that logs both the string and the values returned by each variable equation. 
 
 //define a variable that represents the written text 
 let comboMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
@@ -11,5 +13,5 @@ let num2 = 30 + 10;
 //define a variable that contains the equation for the third combination number 
 let num3 = 45 - 6;
 
-//create an alert message that concatenates the written message and the values of each combination number 
+//create an alert message that concatenates the text and the values of each variable equation 
 alert(comboMessage + " " + num1 + "-"+ num2 + "-" + num3);
