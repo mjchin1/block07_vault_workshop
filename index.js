b763517f66381dd7fc69186e67db6cc57a9f168e
Melissa J. Chin <Melissa@Melissas-MacBook-Air.local>
2023-11-06 12:01:48 -0600

    adding html and js files

---
+++ index.js
@@ -0,0 +1,15 @@
+//1. name three variables. 
+//2. for each variable, create an equation returns a value equal to one of the combination numbers (10, 40 and 39). Use different operators for each equation. 
+//3. Display the results of each equation in a pop-up. 
+
+//define a variable that represents the written text 
+let comboMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//define a variable that contains the equation for the first combination number 
+let num1 = 20/2; 
+//define a variable that contains the equation for the second combination number 
+let num2 = 30 + 10;
+//define a variable that contains the equation for the third combination number 
+let num3 = 45 - 6;
+
+//create an alert message that concatenates the written message and the values of each combination number 
+alert(comboMessage + " " + num1 + "-"+ num2 + "-" + num3);
